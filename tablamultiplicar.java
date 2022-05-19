4540a046ace0dd3c87b888789e15fa60725a5e71
juan fran <1436115@@alu.murciaeduca.es>
2022-05-19 11:13:37 +0200

    tabla de multiplicar commit

---
+++ tablamultiplicar.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class tablamultiplicar{
+	
+	public static void main(String[] args){
+	System.out.println("introduce un numero");
+	Scanner teclado= new Scanner(System.in);
+	int n,resultado;
+	n=teclado.nextInt();
+	for(int fact=1;fact=10;fact++)
+	n*fact=resultado;
+      System.out.println("el resultado es "+resultado);
+		}
+	}
+}
+}
